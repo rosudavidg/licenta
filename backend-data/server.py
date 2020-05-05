081ea5c23a4866070c045b96e5e7bc65e5de8706
Gabriel-David <rosudavidg@gmail.com>
2020-05-05 14:17:06 +0300

    added backend-data server

--- backend-data/server.py
+++ backend-data/server.py
@@ -1,12 +1,31 @@
-from flask import Flask, request
+from facebook_collector import facebook_collect
+from file_system import save_images
+from flask import Flask, request, Response
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/users', methods=['POST'])
 def index():
-    return str('Hello world!.')
+    """Adaugarea unui nou utilizator in sistem"""
+
+    try:
+        # Extrag access-token-ul din cerere
+        token = request.args.get('token')
+
+        # Colectez datele din profilul utilizatorului
+        data = facebook_collect(token)
+
+        # Salveaza imaginile asociate postarilor
+        # save_images(data)
+
+        # TODO: salveaza metadatele in baza de date
+
+    except:
+        return Response.status_code(500)
+
+    return str(data)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")

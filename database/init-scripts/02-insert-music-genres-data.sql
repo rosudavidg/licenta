f28dd348abffb79b0a3a1ba407ac975fd77b0521
Gabriel-David <rosudavidg@gmail.com>
2020-05-07 21:53:00 +0300

    added insert script

---
+++ database/init-scripts/02-insert-music-genres-data.sql
@@ -0,0 +1,13 @@
+-- Adaugarea in baza de date a genurilor muzicale cunoscute
+INSERT INTO music_genres (genre, tags) VALUES ('acoustic', 'acoustic');
+INSERT INTO music_genres (genre, tags) VALUES ('alternative', 'alternative');
+INSERT INTO music_genres (genre, tags) VALUES ('bass', 'bass');
+INSERT INTO music_genres (genre, tags) VALUES ('dubstep', 'dubstep, db');
+INSERT INTO music_genres (genre, tags) VALUES ('edm', 'electronic, dance');
+INSERT INTO music_genres (genre, tags) VALUES ('electronic', 'electronic');
+INSERT INTO music_genres (genre, tags) VALUES ('hip-hop', 'hip-hop, hip, hop, hiphop');
+INSERT INTO music_genres (genre, tags) VALUES ('house', 'house');
+INSERT INTO music_genres (genre, tags) VALUES ('metal', 'metal');
+INSERT INTO music_genres (genre, tags) VALUES ('r&b', 'r&b, rb, r, b, rhythm, blues');
+INSERT INTO music_genres (genre, tags) VALUES ('rap', 'rap');
+INSERT INTO music_genres (genre, tags) VALUES ('rock', 'rock');

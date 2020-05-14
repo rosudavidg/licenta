3e0b43fa296fef423a827f28cf305342866791c2
Gabriel-David <rosudavidg@gmail.com>
2020-05-14 20:07:38 +0300

    added utils file

---
+++ backend-data/utils.py
@@ -0,0 +1,35 @@
+def get_all_image_paths(data):
+
+    # Caile catre imagini
+    paths = []
+
+    # Extrage id-ul utilizatorului
+    user_id = data['profile']['id']
+
+    # Extrage imaginile din albumele utilizatorului
+    albums_photos = data['albums_photos']
+
+    # Compun base path
+    base_path = f'/images/{user_id}'
+
+    # Pentru fiecare album
+    for album_photos in albums_photos:
+        # Pentru fiecare poza din album
+        for photo in album_photos:
+            photo_id = photo['id']
+
+            paths.append(f'{base_path}/{photo_id}/image.jpg')
+
+    # Extrage postarile utilizatorului
+    posts = data['posts']
+
+    # Pentru fiecare postare
+    for post in posts:
+        # Verific daca are poza atasata
+        if 'full_picture' in post:
+            # Extrag id-ul postarii
+            post_id = post['id'].split('_')[1]
+
+            paths.append(f'{base_path}/{post_id}/image.jpg')
+
+    return paths

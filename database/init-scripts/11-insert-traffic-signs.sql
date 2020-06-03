d110e75b782f1534716979ee2c157eaa032602c9
Gabriel-David <rosudavidg@gmail.com>
2020-06-03 18:28:44 +0300

    fixed typo

--- database/init-scripts/11-insert-traffic-signs.sql
+++ database/init-scripts/11-insert-traffic-signs.sql
@@ -4,7 +4,7 @@ INSERT INTO traffic_signs (id, path, name) VALUES (2, '/traffic-signs/1.png', 'D
 INSERT INTO traffic_signs (id, path, name) VALUES (3, '/traffic-signs/2.png', 'Sens giratoriu');
 INSERT INTO traffic_signs (id, path, name) VALUES (4, '/traffic-signs/3.png', 'Interzis virajul la stânga');
 INSERT INTO traffic_signs (id, path, name) VALUES (5, '/traffic-signs/4.png', 'Interzis virajul la dreapta');
-INSERT INTO traffic_signs (id, path, name) VALUES (6, '/traffic-signs/5.png', 'Inainte');
+INSERT INTO traffic_signs (id, path, name) VALUES (6, '/traffic-signs/5.png', 'Înainte');
 INSERT INTO traffic_signs (id, path, name) VALUES (7, '/traffic-signs/6.png', 'Accesul interzis');
 INSERT INTO traffic_signs (id, path, name) VALUES (8, '/traffic-signs/7.png', 'Permis bicicletelor');
 INSERT INTO traffic_signs (id, path, name) VALUES (9, '/traffic-signs/8.png', 'Cedează trecerea');

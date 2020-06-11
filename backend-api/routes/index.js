002be1a4fce1df35a8298f6827d73eda05aea25d
Gabriel-David <rosudavidg@gmail.com>
2020-06-11 22:37:49 +0300

    added status route

--- backend-api/routes/index.js
+++ backend-api/routes/index.js
@@ -3,9 +3,11 @@ const Router = require("express")();
 const UsersController = require("../Users/controller.js");
 const QuestionsController = require("../Questions/controller.js");
 const StatsController = require("../Stats/controller.js");
+const StatusController = require("../Status/controller.js");
 
 Router.use("/users", UsersController);
 Router.use("/questions", QuestionsController);
 Router.use("/stats", StatsController);
+Router.use("/status", StatusController);
 
 module.exports = Router;

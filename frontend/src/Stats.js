5361faa023c85a59ae69da1e4c7f010e2c967154
Gabriel-David <rosudavidg@gmail.com>
2020-06-11 15:52:38 +0300

    updated periodic stats

--- frontend/src/Stats.js
+++ frontend/src/Stats.js
@@ -5,6 +5,8 @@ import "./Stats.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowCircleLeft, faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
 
+import { HorizontalBar, Bar, Line, Pie, Doughnut } from "react-chartjs-2";
+
 const StatsTop = (props) => {
   return (
     <div className="stats-top">
@@ -34,7 +36,7 @@ const StatsTop = (props) => {
   );
 };
 
-const getStats = async (setStats) => {
+const getStats = async (setStats, setReady) => {
   const jwt_token = localStorage.getItem("token");
 
   axios
@@ -45,6 +47,7 @@ const getStats = async (setStats) => {
     })
     .then((res) => {
       setStats(res.data);
+      setReady(true);
     })
     .catch((e) => {
       alert(`Get question failed!\nError: ${e.response.data.error}`);
@@ -89,15 +92,187 @@ const Periodic = (props) => {
           <FontAwesomeIcon icon={faArrowCircleRight} />
         </div>
       </div>
+      <div className="stats-periodic-stats-total">
+        <div className="stats-periodic-stats-pie">
+          <Doughnut
+            options={{
+              title: {
+                display: true,
+                text: "Precizia totală",
+                fontColor: "#cdd1c4",
+                fontSize: 16,
+              },
+              legend: {
+                display: false,
+              },
+            }}
+            data={{
+              labels: ["Corect", "Greșit"],
+              datasets: [
+                {
+                  data: [
+                    Math.round(stats[index]["acc_total"] * 100),
+                    100 - Math.round(stats[index]["acc_total"] * 100),
+                  ],
+                  backgroundColor: ["#cdd1c4", "#22333b"],
+                  hoverBackgroundColor: ["4e8098", "#22333b"],
+                },
+              ],
+            }}
+          />
+          <div className="stats-periodic-stats-pie-text">{Math.round(stats[index]["acc_total"] * 100)}%</div>
+        </div>
+        <div className="stats-periodic-stats-pie">
+          <Doughnut
+            options={{
+              title: {
+                display: true,
+                text: "Memorie de scurtă durată",
+                fontColor: "#cdd1c4",
+                fontSize: 16,
+              },
+              legend: {
+                display: false,
+              },
+            }}
+            data={{
+              labels: ["Corect", "Greșit"],
+              datasets: [
+                {
+                  data: [
+                    Math.round(stats[index]["acc_short_term_memory"] * 100),
+                    100 - Math.round(stats[index]["acc_short_term_memory"] * 100),
+                  ],
+                  backgroundColor: ["#cdd1c4", "#22333b"],
+                  hoverBackgroundColor: ["4e8098", "#22333b"],
+                },
+              ],
+            }}
+          />
+          <div className="stats-periodic-stats-pie-text">
+            {Math.round(stats[index]["acc_short_term_memory"] * 100)}%
+          </div>
+        </div>
+        <div className="stats-periodic-stats-pie">
+          <Doughnut
+            options={{
+              title: {
+                display: true,
+                text: "Memorie de lungă durată",
+                fontColor: "#cdd1c4",
+                fontSize: 16,
+              },
+              legend: {
+                display: false,
+              },
+            }}
+            data={{
+              labels: ["Corect", "Greșit"],
+              datasets: [
+                {
+                  data: [
+                    Math.round(stats[index]["acc_long_term_memory"] * 100),
+                    100 - Math.round(stats[index]["acc_long_term_memory"] * 100),
+                  ],
+                  backgroundColor: ["#cdd1c4", "#22333b"],
+                  hoverBackgroundColor: ["4e8098", "#22333b"],
+                },
+              ],
+            }}
+          />
+          <div className="stats-periodic-stats-pie-text">{Math.round(stats[index]["acc_long_term_memory"] * 100)}%</div>
+        </div>
+      </div>
+      <div className="stats-periodic-stats">
+        <div className="stats-periodic-stats-text">Precizii detaliate</div>
+        <HorizontalBar
+          className="stats-periodic-stats-bar"
+          options={{
+            legend: {
+              display: false,
+            },
+            scales: {
+              xAxes: [
+                {
+                  ticks: {
+                    fontColor: "#cdd1c4",
+                    fontSize: 16,
+                    beginAtZero: true,
+                    min: 0,
+                    max: 100,
+                  },
+                },
+              ],
+              yAxes: [
+                {
+                  ticks: {
+                    fontColor: "#cdd1c4",
+                    fontSize: 16,
+                  },
+                },
+              ],
+            },
+          }}
+          data={{
+            labels: [
+              "Memorare cuvinte",
+              "Recunoaștere persoane",
+              "Anotimpuri",
+              "Ziua din săptămână",
+              "Indicatoare rutiere",
+              "Zi de naștere",
+              "Data calendaristică",
+              "Recunoaștere animale",
+              "Joc de memorie",
+              "Zaruri",
+              "Desenare ceas",
+              "Oraș natal",
+              "Oraț curent",
+              "Limbi cunoscute",
+              "Genuri muzicale",
+              "Postări",
+              "Cărți",
+              "Filme",
+            ],
 
-      <div className="stats-periodic-stats">Precizia totală {stats[index]["acc_total"]}</div>
+            datasets: [
+              {
+                // barPercentage: 0.5,
+                data: [
+                  Math.round(stats[index]["acc_common_words"] * 100),
+                  Math.round(stats[index]["acc_face"] * 100),
+                  Math.round(stats[index]["acc_season"] * 100),
+                  Math.round(stats[index]["acc_today"] * 100),
+                  Math.round(stats[index]["acc_traffic_sign"] * 100),
+                  Math.round(stats[index]["acc_birthday"] * 100),
+                  Math.round(stats[index]["acc_today_date"] * 100),
+                  Math.round(stats[index]["acc_animals"] * 100),
+                  Math.round(stats[index]["acc_memory_game"] * 100),
+                  Math.round(stats[index]["acc_dices"] * 100),
+                  Math.round(stats[index]["acc_clock"] * 100),
+                  Math.round(stats[index]["acc_hometown"] * 100),
+                  Math.round(stats[index]["acc_location"] * 100),
+                  Math.round(stats[index]["acc_language"] * 100),
+                  Math.round(stats[index]["acc_music_genre"] * 100),
+                  Math.round(stats[index]["acc_post"] * 100),
+                  Math.round(stats[index]["acc_book"] * 100),
+                  Math.round(stats[index]["acc_movie"] * 100),
+                ],
+                backgroundColor: "#cdd1c4",
+                hoverBackgroundColor: "#4e8098",
+              },
+            ],
+          }}
+        />
+      </div>
     </>
   );
 };
 
 const Stats = () => {
-  const [selectedMenu, setSelectedMenu] = useState("evolutie");
+  const [selectedMenu, setSelectedMenu] = useState("periodic");
   const [stats, setStats] = useState({});
+  const [ready, setReady] = useState(false);
 
   const onClickEvolutie = () => {
     setSelectedMenu("evolutie");
@@ -108,13 +283,13 @@ const Stats = () => {
   };
 
   useEffect(() => {
-    getStats(setStats);
+    getStats(setStats, setReady);
   }, []);
 
   return (
     <div className="stats-container">
       <StatsTop onClickEvolutie={onClickEvolutie} onClickPeriodic={onClickPeriodic} selectedMenu={selectedMenu} />
-      {selectedMenu === "periodic" && <Periodic stats={stats} />}
+      {ready && selectedMenu === "periodic" && <Periodic stats={stats} />}
     </div>
   );
 };
